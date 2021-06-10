250b704e71fabcd64260c6d7446e27617651aaa5
guille-block <fairbairnguillermo@gmail.com>
2021-06-10 09:19:27 -0300

    Updates

---
+++ precache-manifest.cbbe83426c2109b16d5cf3f9799ae080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b3dd47ce43e141f2bd7",
+    "url": "/staker/static/css/main.68447cda.chunk.css"
+  },
+  {
+    "revision": "4b3dd47ce43e141f2bd7",
+    "url": "/staker/static/js/main.4b3dd47c.chunk.js"
+  },
+  {
+    "revision": "b5511b077172e7cf576a",
+    "url": "/staker/static/css/1.14a4bb44.chunk.css"
+  },
+  {
+    "revision": "b5511b077172e7cf576a",
+    "url": "/staker/static/js/1.b5511b07.chunk.js"
+  },
+  {
+    "revision": "d10c5435cd447cc87f4d",
+    "url": "/staker/static/js/runtime~main.d10c5435.js"
+  },
+  {
+    "revision": "8dd66f71c32d488de50a39ee2dea153d",
+    "url": "/staker/index.html"
+  }
+];
